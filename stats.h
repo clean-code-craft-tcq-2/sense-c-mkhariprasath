d1c5f68a898f4dfd84f19eb9b7cfd914bb280ac2
Hariprasath Muthumanickam <39271101+mkhariprasath@users.noreply.github.com>
2021-10-18 22:58:10 +0530

    moved alerter functions to respective files

--- stats.h
+++ stats.h
@@ -8,12 +8,6 @@ typedef struct Stats
 
 struct Stats compute_statistics(const float* numberset, int setlength);
 
-typedef void (*alerter_funcptr)();
-void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats);
-
-extern int emailAlertCallCount;
-extern int ledAlertCallCount;
-
 float min(const float* numberset, int setlength);
 float max(const float* numberset, int setlength);
 float avg(const float* numberset, int setlength);
