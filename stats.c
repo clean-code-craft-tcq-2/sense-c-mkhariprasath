d2c36ea210adca2b6a872dc0a39ea03ce1ccc7c2
Hariprasath Muthumanickam <39271101+mkhariprasath@users.noreply.github.com>
2021-10-18 19:09:43 +0530

    First try

--- stats.c
+++ stats.c
@@ -1,10 +1,58 @@
-#include "stats.h"
+struct Stats compute_statistics(const float* numberset, int setlength);
+
+typedef void (*alerter_funcptr)();
+void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats);
+
+extern int emailAlertCallCount;
+extern int ledAlertCallCount;
+
+float min(const float* numberset, int setlength);
+float max(const float* numberset, int setlength);
+float avg(const float* numberset, int setlength);
+    check_and_alert(maxThreshold, alerters, computedStats);
+
+float min(const float* numberset, int setlength)
+{
+    float ret = numberset[0];
+    for (int i = 1; i < setlength; i++)
+    {
+        if (ret < numberset[i])
+        {
+            ret = numberset[i];
+        }
+    }
+    return (float)ret;
+}
+
+float max(const float* numberset, int setlength)
+{
+    float ret = numberset[0];
+    for (int i = 1; i < setlength; i++)
+    {
+        if (ret > numberset[i])
+        {
+            ret = numberset[i];
+        }
+    }
+    return (float)ret;
+}
+float avg(const float* numberset, int setlength)
+{
+    float ret = numberset[0];
+    for (int i = 0; i < setlength; i++)
+    {
+        ret = ret + numberset[i];
+    }
+    return (float)(ret/setlength);
+}
 
 struct Stats compute_statistics(const float* numberset, int setlength) {
+        
     struct Stats s;
-    s.average = 0;
-    s.min = 0;
-    s.max = 0;
+    s.average = avg(const float* numberset, int setlength);
+    s.min = min(const float* numberset, int setlength);
+    s.max = max(const float* numberset, int setlength);
+    return s;
 }
 
 int emailAlertCallCount = 0;
